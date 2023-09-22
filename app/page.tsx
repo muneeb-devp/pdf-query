ae87fef2803797f831beb8eddcb558af4624d86d
Muneeb Mughal <muneeb.devp@gmail.com>
2023-09-22 23:58:45 +0500

    Added Clerk Auth Provider to layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-red-600 text-4xl"> Hello World</h1>
-    <Button> Hello World</Button>
+      <Button> Click Me</Button>
     </>
   )
 }
