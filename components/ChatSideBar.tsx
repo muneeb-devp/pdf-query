ebdd9b58ba03839bfe45f4d06e4b17f4081636d2
Muneeb Mughal <muneeb.devp@gmail.com>
2025-01-03 15:29:23 +0500

    Added upgrade to pro functionality

--- components/ChatSideBar.tsx
+++ components/ChatSideBar.tsx
@@ -1,22 +1,35 @@
-'use client'
+'use client';
 
-import React from 'react'
+import React from 'react';
 
-import { DrizzleChat } from '@/lib/db/schema'
-import Link from 'next/link'
-import { Button } from './ui/button'
-import { MessageCircle, PlusCircle } from 'lucide-react'
-import { cn } from '@/lib/utils'
+import { DrizzleChat } from '@/lib/db/schema';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { MessageCircle, PlusCircle } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+const handlePurchase = async () => {
+  const resp = await fetch('/api/purchase', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ productId: '646028' }),
+  });
+
+  const { url } = await resp.json();
+  window.location.href = url;
+};
 
 type Props = {
-  chats: DrizzleChat[]
-  chatId: number
-}
+  chats: DrizzleChat[];
+  chatId: number;
+};
 
 const ChatSideBar = ({ chats, chatId }: Props) => {
-  console.log(chats)
+  console.log(chats);
   return (
-    <div className='w-full h-screen p-4 text-gray-200 bg-gray-900'>
+    <div className='w-full h-screen flex flex-col p-4 text-gray-200 bg-gray-900'>
       <Link href='/'>
         <Button className='w-full border-dashed border-white border'>
           <PlusCircle className='mr-2 w-4 h-4' />
@@ -24,8 +37,8 @@ const ChatSideBar = ({ chats, chatId }: Props) => {
         </Button>
       </Link>
 
-      <div className='flex flex-col gap-2 mt-4'>
-        {chats.map(chat => (
+      <div className='flex flex-col grow gap-2 mt-4'>
+        {chats.map((chat) => (
           <Link key={chat.id} href={`/chat/${chat.id}`}>
             <div
               className={cn('rounded-lg p-3 text-slate-300 flex items-center', {
@@ -42,14 +55,19 @@ const ChatSideBar = ({ chats, chatId }: Props) => {
         ))}
       </div>
 
-      <div className='absolute bottom-4 left-4'>
-        <div className='flex items-center gap-2 text-sm text-slate-500 flex-wrap'>
-          <Link href='/'>Home</Link>
-          <Link href='/'>Source</Link>
-        </div>
+      <div className='flex flex-col'>
+        <Button
+          onClick={handlePurchase}
+          className=' bg-orange-400 hover:bg-orange-500'
+        >
+          Upgrade to PRO
+        </Button>
+        <Link href='/' className='block mt-2 text-center text-white'>
+          Home
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatSideBar
+export default ChatSideBar;
