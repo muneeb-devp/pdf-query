72643322f95fe00cf8dfc8886054d7ade442501a
Muneeb Mughal <muneeb.devp@gmail.com>
2025-01-03 15:28:36 +0500

    Limit free tier queries to 3

--- components/ChatComponent.tsx
+++ components/ChatComponent.tsx
@@ -1,17 +1,18 @@
-'use client'
+'use client';
+import toast from 'react-hot-toast';
 
-import React from 'react'
+import React, { useEffect } from 'react';
 
-import { Input } from './ui/input'
-import { useChat } from 'ai/react'
-import { Button } from './ui/button'
-import { Send } from 'lucide-react'
-import MessageList from './MessageList'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
-import { Message } from 'ai'
+import { Input } from './ui/input';
+import { useChat } from 'ai/react';
+import { Button } from './ui/button';
+import { Send } from 'lucide-react';
+import MessageList from './MessageList';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { Message } from 'ai';
 
-type Props = { chatId: number }
+type Props = { chatId: number };
 
 const ChatComponent = ({ chatId }: Props) => {
   const { data, isLoading } = useQuery({
@@ -19,32 +20,45 @@ const ChatComponent = ({ chatId }: Props) => {
     queryFn: async () => {
       const response = await axios.post<Message[]>('/api/get-messages', {
         chatId,
-      })
-      return response.data
+      });
+      return response.data;
     },
-  })
+  });
 
-  const { input, handleInputChange, handleSubmit, messages } = useChat({
+  const { input, handleInputChange, handleSubmit, error, messages } = useChat({
     api: '/api/chat',
     body: {
       chatId,
     },
     initialMessages: data || [],
-  })
+  });
+
+  useEffect(() => {
+    if (error) {
+      const { error: errorMsg } = JSON.parse(error.message);
+
+      if (errorMsg === 'free tier limit reached') {
+        toast.error(errorMsg);
+        toast.error('Switch to pro plan to have unlimited access', {
+          duration: 10_000,
+        });
+      }
+    }
+  }, [error]);
 
   React.useEffect(() => {
-    const messageContainer = document.querySelector('#message-container')
+    const messageContainer = document.querySelector('#message-container');
 
     if (messageContainer)
       messageContainer.scrollTo({
         top: messageContainer.scrollHeight,
         behavior: 'smooth',
-      })
-  }, [messages])
+      });
+  }, [messages]);
 
   return (
     <div
-      className='relative max-h-screen min-h-screen overflow-scroll'
+      className='relative max-h-screen min-h-screen overflow-scroll flex flex-col'
       id='message-container'
     >
       {/* Header */}
@@ -52,25 +66,29 @@ const ChatComponent = ({ chatId }: Props) => {
         <h3 className='text-xl font-bold'>Chat</h3>
       </div>
 
-      {/* Msg List */}
-      <MessageList messages={messages} isLoading={isLoading} />
+      <div className='flex-1 overflow-scroll'>
+        {/* Msg List */}
+        <MessageList messages={messages} isLoading={isLoading} />
+      </div>
 
       <form
         onSubmit={handleSubmit}
-        className='sticky flex bottom-0 inset-x-0 px-2 py-4 bg-white'
+        className='sticky flex  bottom-0 inset-x-0 px-2 py-4 bg-white'
       >
         <Input
           value={input}
-          onChange={handleInputChange}
+          onChange={async (e) => {
+            handleInputChange(e);
+          }}
           placeholder='Ask any question...'
           className='w-full'
         />
-        <Button className='bg-blue-600 ml-2'>
+        <Button className='bg-blue-600  ml-2'>
           <Send className='h-4 w-4' />
         </Button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default ChatComponent
+export default ChatComponent;
