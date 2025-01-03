9a4904a11f0ce31ce56cc72651821989b833dda6
Muneeb Mughal <muneeb.devp@gmail.com>
2025-01-03 15:25:26 +0500

    added premium subscription api endpoint

---
+++ app/api/purchase/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server"
+import { lemonInstance } from "@/lib/lemonsqueezy"
+import { auth, currentUser, getAuth } from '@clerk/nextjs/server';
+
+export const dynamic = "force-dynamic"
+
+export async function POST(req: Request) {
+  try {
+    const data = await req.json()
+    if (!data.productId)
+      return NextResponse.json({ error: "productId is required" }, { status: 400 })
+
+    const { userId } = await auth()
+    const response = await lemonInstance.post('/checkouts', {
+      data: {
+        type: "checkouts",
+        attributes: {
+          checkout_data: {
+            custom: {
+              userId: `${userId}`
+            }
+          },
+        },
+        relationships: {
+          store: {
+            data: {
+              type: "stores",
+              id: `${process.env.LEMONSQUEEZY_STORE_ID}`
+            }
+          },
+          variant: {
+            data: {
+              type: "variants",
+              id: `${data.productId}`
+            }
+          }
+        }
+      }
+    })
+    return NextResponse.json({ url: response.data.data.attributes.url }, { status: 200, statusText: 'OK' })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
