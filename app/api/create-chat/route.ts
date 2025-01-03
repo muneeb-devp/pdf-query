2eea3df5a062ec8894b13d171cda195d3c8a4411
Muneeb Mughal <muneeb.devp@gmail.com>
2025-01-04 04:24:08 +0500

    Removed redundant response parameter

--- app/api/create-chat/route.ts
+++ app/api/create-chat/route.ts
@@ -5,7 +5,7 @@ import { loadAzureBlobIntoPinecone } from '@/lib/pinecone'
 import { auth } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
   try {
     const body = await req.json()
     const { fileKey, fileName } = body
