9dbe9a44b89ea329a5d78adf202589111ead0c36
Muneeb Mughal <muneeb.devp@gmail.com>
2025-01-03 15:15:39 +0500

    corrected nesting structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,35 @@
-import './globals.css'
-import ReactQueryProvider from '@/components/ReactQueryProvider'
+import './globals.css';
+import ReactQueryProvider from '@/components/ReactQueryProvider';
 
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import { Toaster } from 'react-hot-toast'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from 'react-hot-toast';
+import { data } from '@/lib/lemonsqueezy';
 
-const inter = Inter({ subsets: ['latin'] })
+console.log(data);
+
+const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'PDF Query',
   description: 'An AI powered chat assistant to query any given PDF document',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <ReactQueryProvider>
-        <html lang='en-us'>
-          <body className={inter.className}>{children}</body>
-          <Toaster position='top-right' toastOptions={{ duration: 5_000 }} />
-        </html>
-      </ReactQueryProvider>
-    </ClerkProvider>
-  )
+    <html lang='en-us'>
+      <body className={inter.className}>
+        <ClerkProvider>
+          <ReactQueryProvider>
+            {children}
+            <Toaster position='top-right' toastOptions={{ duration: 5_000 }} />
+          </ReactQueryProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
