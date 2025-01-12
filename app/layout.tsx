b111a1480343d6ea01497b36537c3f1ffcc9e836
Muneeb Mughal <muneeb.devp@gmail.com>
2025-01-12 20:48:05 +0500

    initialize ms-clarity for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,14 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from 'react-hot-toast';
+import Clartiy from '@microsoft/clarity';
 
 const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'PDF Query',
   description: 'An AI powered chat assistant to query any given PDF document',
 };
+Clartiy.init(process.env.CLARITY_PROJECT_ID || '');
 
 export default function RootLayout({
   children,
